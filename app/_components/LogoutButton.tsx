25d993add1c41c42c27d50575b7c3b63c568de01
exploitenomah <iksploiting@gmail.com>
2024-04-01 17:35:14 +0100

    refactor

--- app/_components/LogoutButton.tsx
+++ app/_components/LogoutButton.tsx
@@ -1,24 +1,37 @@
-import { Button } from "@chakra-ui/react";
-import localforage from "localforage";
-import { useRouter } from "next/navigation";
-import { ReactNode, useCallback } from "react";
-import STORAGE_KEYS from "../STORAGE_KEYS";
+import { Button } from "@chakra-ui/react"
+import localforage from "localforage"
+import { useRouter } from "next/navigation"
+import { ReactNode, useCallback } from "react"
+import STORAGE_KEYS from "../STORAGE_KEYS"
 
-
-export default function LogoutButton({ children, onLogout }: {
+export default function LogoutButton({
+  children,
+  onLogout,
+}: {
   children: ReactNode | ReactNode[]
   onLogout?: () => void
-}){
-
+}) {
   const router = useRouter()
   const logout = useCallback(async () => {
-    await localforage.removeItem(STORAGE_KEYS.BA_TOKEN)
-    if(typeof onLogout === "function") onLogout()
+    Promise.all(
+      Object.keys(STORAGE_KEYS).map(
+        async (key) => await localforage.removeItem(key)
+      )
+    ).catch(console.log)
+    if (typeof onLogout === "function") onLogout()
     router.push("/")
   }, [onLogout, router])
 
   return (
-    <Button _hover={{ bg: "transparent" }} _focus={{ border:"", boxShadow: "none", outline: "0"}} flexGrow="1" w="max-content" bg="transparent" h="unset" onClick={logout}>
+    <Button
+      _hover={{ bg: "transparent" }}
+      _focus={{ border: "", boxShadow: "none", outline: "0" }}
+      flexGrow="1"
+      w="max-content"
+      bg="transparent"
+      h="unset"
+      onClick={logout}
+    >
       {children}
     </Button>
   )
