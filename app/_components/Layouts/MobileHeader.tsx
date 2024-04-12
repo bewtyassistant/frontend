16c29653fd5aed9c7a5cf234a000c0c8d64e0f17
exploitenomah <iksploiting@gmail.com>
2024-04-12 23:59:57 +0100

    restyle and refactor

--- app/_components/Layouts/MobileHeader.tsx
+++ app/_components/Layouts/MobileHeader.tsx
@@ -1,11 +1,13 @@
 import { Box, Flex, IconButton } from "@chakra-ui/react"
 import { LegacyRef, ReactNode, RefObject } from "react"
 import MenuIcon from "@/app/_assets/MenuIcon"
+import NotificationButton from "../NotificationButton"
+import MessageButton from "../MessageButton"
 
 export default function MobileHeader({
   children,
   toggleButtonRef,
-  toggleSideBar
+  toggleSideBar,
 }: {
   children: ReactNode | ReactNode[]
   toggleButtonRef?: RefObject<HTMLButtonElement | null>
@@ -19,17 +21,20 @@ export default function MobileHeader({
       px="2rem"
     >
       <Box flexGrow="1">{children}</Box>
-      <Box>
+      <Flex alignItems="center" gap="1.6rem">
+        <NotificationButton />
+        <MessageButton />
         <IconButton
           onClick={toggleSideBar}
           ref={toggleButtonRef as LegacyRef<HTMLButtonElement>}
           aria-label="toggle-menu"
           color="brand.main"
           bg="transparent"
+          ml=".5rem"
         >
           <MenuIcon />
         </IconButton>
-      </Box>
+      </Flex>
     </Flex>
   )
 }
