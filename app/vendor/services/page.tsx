c233a784f0e2facda86415894079f2f189f7804b
exploitenomah <iksploiting@gmail.com>
2024-10-21 10:02:17 +0100

    create services page for vendor

---
+++ app/vendor/services/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import BasicPageLayout from "@/app/_components/Layouts/BasicPageLayout"
+import MobileSearchHeader from "@/app/_components/Layouts/MobileSearchHeader"
+
+export default function VendorOverviewPage() {
+  return (
+    <BasicPageLayout>
+      <MobileSearchHeader />
+    </BasicPageLayout>
+  )
+}
