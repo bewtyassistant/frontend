02acf3156f632d9ce80ab49b5ef9b57727c71af4
exploitenomah <iksploiting@gmail.com>
2025-01-24 12:38:45 +0100

    change style of logout button to be secondary style

--- app/_components/NavLogoutButton.tsx
+++ app/_components/NavLogoutButton.tsx
@@ -1,16 +1,21 @@
-import { Button } from "@chakra-ui/react";
-import LogoutButton from "./LogoutButton";
+import { Button } from "@chakra-ui/react"
+import LogoutButton from "./LogoutButton"
 
-
-export default function NavLogoutButton(){
+export default function NavLogoutButton() {
   return (
     <LogoutButton>
       <Button
         as="span"
         w="40dvw"
         maxW="12.7rem"
-        variant="filled"
-        borderRadius=".4rem"
+        variant="outline"
+        color={{ base: "white", md: "brand.main" }}
+        borderColor="currentcolor"
+        padding={{ base: "20px 0", md: "16px 0" }}
+        bg={{ base: "brand.main", md: "white" }}
+        borderRadius={{ base: "2.4rem", md: ".4rem" }}
+        height="unset"
+        _hover={{ bg: "#f4afb4", color: "brand.main" }}
       >
         Logout
       </Button>
